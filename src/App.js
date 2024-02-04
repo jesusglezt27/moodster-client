f9b331d902831b271016550974d2d8ea655bbb94
Jesus Gonzalez <jesusglezt27@gmail.com>
2024-02-04 15:16:07 -0600

    auth

--- src/App.js
+++ src/App.js
@@ -5,11 +5,9 @@ import Callback from './components/callback/Callback';
 import Login from './components/login/Login';
 import MoodAndArtistSelection from './components/MoodAndArtistSelection/MoodAndArtistSelection';
 import Playlist from './components/playlist/Playlist';
-import { useAuth } from './components/context/AuthContext'; // Ajusta la ruta según tu estructura de archivos
 import {AuthProvider} from './components/context/AuthContext';
-function App() {
-  const { authToken } = useAuth();
 
+function App() {
   return (
     <AuthProvider>
       <Router>
